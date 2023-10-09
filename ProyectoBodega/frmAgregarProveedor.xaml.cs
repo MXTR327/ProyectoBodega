44f91a8f4928c4d2ff0c4aac1c8b85bec3ba21c2
MXTR327 <111721078+MXTR327@users.noreply.github.com>
2023-10-09 12:40:02 -0500

    agregar diseño a frmAgregarProveedor

--- ProyectoBodega/frmAgregarProveedor.xaml.cs
+++ ProyectoBodega/frmAgregarProveedor.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using Negocio;
 using System.Data;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Forms;
 using System.Windows.Input;
-using MessageBox = System.Windows.Forms.MessageBox;
-using TextBox = System.Windows.Controls.TextBox;
 
 namespace ProyectoBodega
 {
@@ -27,21 +25,29 @@ namespace ProyectoBodega
                 txtCodigo.Visibility = Visibility.Visible;
                 lblCodigo.Visibility = Visibility.Visible;
 
-                DataRowView filaSeleccionada = (DataRowView)ventanaProducto.dgProveedor.SelectedItem;
 
+                txtbNombre.Visibility = Visibility.Collapsed;
+
+                if (string.IsNullOrWhiteSpace(txtDireccion.Text))
+                {
+                    txtbDireccion.Visibility = Visibility.Collapsed;
+                }
+                if (string.IsNullOrWhiteSpace(txtNumero.Text))
+                {
+                    txtbNumero.Visibility = Visibility.Collapsed;
+                }
+                //------------------------------------------------------------------------------------------------------------------------------\\
+                DataRowView filaSeleccionada = (DataRowView)ventanaProducto.dgProveedor.SelectedItem;
                 string Id = filaSeleccionada["idProveedor"].ToString();
                 string nombre = filaSeleccionada["nombre_proveedor"].ToString();
                 string direccion = filaSeleccionada["direccion_proveedor"].ToString();
                 string numero = filaSeleccionada["numero_contacto"].ToString();
-
                 nombreProveedor_primero = nombre;
-
                 txtCodigo.Text = Id;
                 txtNombre.Text = nombre;
-
                 if (!string.IsNullOrEmpty(numero))
                 {
-                    chkContacto.IsChecked = true;
+                    chkNumero.IsChecked = true;
                     txtNumero.IsReadOnly = false;
                     txtNumero.Text = numero;
                 }
@@ -52,17 +58,38 @@ namespace ProyectoBodega
 
                     txtDireccion.Text = direccion;
                 }
+                txtNombre.Focus();
             }
         }
         private void chkDireccion_Click(object sender, RoutedEventArgs e)
         {
             txtDireccion.Text = "";
             txtDireccion.IsReadOnly = !txtDireccion.IsReadOnly;
+            if (txtDireccion.IsReadOnly == false)
+            {
+                gridDireccion.Cursor = Cursors.IBeam;
+                txtDireccion.Focus();
+            }
+            else
+            {
+                gridDireccion.Cursor = Cursors.Arrow;
+                txtbDireccion.Visibility = Visibility.Visible;
+            }
         }
-        private void chkContacto_Click(object sender, RoutedEventArgs e)
+        private void chkNumero_Click(object sender, RoutedEventArgs e)
         {
             txtNumero.Text = "";
             txtNumero.IsReadOnly = !txtNumero.IsReadOnly;
+            if (txtNumero.IsReadOnly == false)
+            {
+                gridNumero.Cursor = Cursors.IBeam;
+                txtNumero.Focus();
+            }
+            else
+            {
+                gridNumero.Cursor = Cursors.Arrow;
+                txtbNumero.Visibility = Visibility.Visible;
+            }
         }
         private void btnSalir_Click(object sender, RoutedEventArgs e)
         {
@@ -74,7 +101,7 @@ namespace ProyectoBodega
         {
             if (string.IsNullOrWhiteSpace(txtNombre.Text))
             {
-                MessageBox.Show("No completó el campo obligatorio Nombre", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("No completó el campo obligatorio Nombre", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 txtNombre.Focus();
                 return;
             }
@@ -96,7 +123,7 @@ namespace ProyectoBodega
                             ventanaProducto.CargarProveedor();
                             ventanaProducto.CargarlistaProveedor();
                         }
-                        MessageBox.Show("El proveedor se subió correctamente", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        MessageBox.Show("El proveedor se subió correctamente", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
                         txtNombre.Text = "";
                         txtDireccion.Text = "";
                         txtNumero.Text = "";
@@ -104,13 +131,13 @@ namespace ProyectoBodega
                     }
                     else
                     {
-                        MessageBox.Show("Error al intentar Insertar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show("Error al intentar Insertar", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                         txtNombre.Focus();
                     }
                 }
                 else
                 {
-                    MessageBox.Show("El proveedor ya existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("El proveedor ya existe", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                     txtNombre.Focus();
                 }
             }
@@ -120,7 +147,7 @@ namespace ProyectoBodega
                 {
                     if (!cn_frmproveedor.VerificarExistencia(nombreProveedor))
                     {
-                        MessageBox.Show("El proveedor ya existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show("El proveedor ya existe", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                         txtNombre.Focus();
                         return;
                     }
@@ -132,19 +159,19 @@ namespace ProyectoBodega
                         ventanaProducto.CargarProveedor();
                         ventanaProducto.CargarlistaProveedor();
                     }
-                    MessageBox.Show("La Categoria se Actualizó correctamente", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("La Categoria se Actualizó correctamente", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
                     nombreProveedor_primero = nombreProveedor;
                     txtNombre.Focus();
                 }
                 else
                 {
-                    MessageBox.Show("Error al intentar Actualizar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Error al intentar Actualizar", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                     txtNombre.Focus();
                 }
             }
         }
         //------------------------------------------------------------------------------------------------------------------------------\\
-        private void txtNombre_TextChanged(object sender, TextChangedEventArgs e)
+        private void primeraLetraMayuscula_TextChanged(object sender, TextChangedEventArgs e)
         {
             TextBox textBox = (TextBox)sender;
 
@@ -155,15 +182,11 @@ namespace ProyectoBodega
                 textBox.SelectionStart = textBox.Text.Length;
             }
         }
-        private void txtDireccion_TextChanged(object sender, TextChangedEventArgs e)
+        private void txtTelefono_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            TextBox textBox = (TextBox)sender;
-
-            if (!string.IsNullOrEmpty(textBox.Text))
+            if (!char.IsDigit((char)KeyInterop.VirtualKeyFromKey(e.Key)) && e.Key != Key.Back || (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control && e.Key == Key.V)
             {
-                string newText = char.ToUpper(textBox.Text[0]) + textBox.Text.Substring(1).ToLower();
-                textBox.Text = newText;
-                textBox.SelectionStart = textBox.Text.Length;
+                e.Handled = true;
             }
         }
         //------------------------------------------------------------------------------------------------------------------------------\\
@@ -174,7 +197,68 @@ namespace ProyectoBodega
                 btnAgregarProveedor_Click(sender, e);
             }
         }
+        //------------------------------------------------------------------------------------------------------------------------------\\
+        private void nombre_Click(object sender, MouseButtonEventArgs e)
+        {
+            txtNombre.Focus();
+        }
+        private void txtNombre_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtbNombre.Visibility = Visibility.Collapsed;
 
-
+            if (string.IsNullOrWhiteSpace(txtDireccion.Text))
+            {
+                txtbDireccion.Visibility = Visibility.Visible;
+            }
+            if (string.IsNullOrWhiteSpace(txtNumero.Text))
+            {
+                txtbNumero.Visibility = Visibility.Visible;
+            }
+        }
+        private void txtNombre_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                txtbNombre.Visibility = Visibility.Visible;
+            }
+        }
+        //------------------------------------------------------------------------------------------------------------------------------\\
+        private void direccion_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (chkDireccion.IsChecked == true)
+            {
+                txtDireccion.Focus();
+            }
+        }
+        private void txtDireccion_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtbDireccion.Visibility = Visibility.Collapsed;
+        }
+        private void txtDireccion_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtDireccion.Text))
+            {
+                txtbDireccion.Visibility = Visibility.Visible;
+            }
+        }
+        //------------------------------------------------------------------------------------------------------------------------------\\
+        private void numero_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (chkNumero.IsChecked == true)
+            {
+                txtNumero.Focus();
+            }
+        }
+        private void txtNumero_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtbNumero.Visibility = Visibility.Collapsed;
+        }
+        private void txtNumero_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNumero.Text))
+            {
+                txtbNumero.Visibility = Visibility.Visible;
+            }
+        }
     }
 }
